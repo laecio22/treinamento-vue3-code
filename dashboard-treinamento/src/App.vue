9a05cc6deca9b99fca003c96419a278fd9b63adf
laecio22 <laecon22@hotmail.com>
2023-10-15 12:49:18 -0300

    chore:configuração inicial  do  projeto

---
+++ dashboard-treinamento/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+      <h1>Hello world</h1>      
+      <ul>
+        <li><a href="#">link</a></li>
+        <li>all</li>
+        <li>all</li>
+        <li>all</li>
+        <li>all</li>
+      </ul>
+  </div> 
+</template>
+
+<style scoped>
+</style>
