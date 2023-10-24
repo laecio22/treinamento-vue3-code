56215af7bc847ec3c8ad7685033dc5aa18f1623a
laecio22 <laecon22@hotmail.com>
2023-10-24 12:19:48 -0300

    feat:Inicio  da  implementação  e  estilisação  da  página  Home

---
+++ dashboard-treinamento/src/components/Contact.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex justify-center w-full">
+      <div class="flex flex-col items-center w-4/5 max-w-6xl my-16">
+        <h1 class="text-5xl font-black">Alguma dúvida?</h1>
+        <p class="font-normal text-2xl text-gray-700">Quer  saber melhor como  funciona e  quais são os  preços?</p>
+         <div class="mt-12 ">
+             <a href="mailto:" class="font-bold text-lg bg-pink-500 rounded-full text-white px-10 py-3 mt-5">Nos mande um  email</a>
+         </div>       
+      </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+
--- dashboard-treinamento/src/views/Home.vue
+++ dashboard-treinamento/src/views/Home.vue
@@ -1,10 +1,38 @@
 <template>
     <div>
-       Aqui  é  a  página  Home
+        <CustomHeader
+        @login="handleLogin"
+        @createAccount="handleCreateAccount"
+         />
+        <Contact />
+        <div class="flex justify-center py-10 bg-gray-100">
+            <p class="font-medium text-center text-gray-800 "> copyright 2023 </p>           
+        </div>     
     </div>
 </template>
 
 <script setup lang="ts">
+ import CustomHeader from '../components/CustomHeader.vue'
+ import Contact from '../components/Contact.vue'
+ import { onMounted } from 'vue';
+ import { useRouter } from 'vue-router';
 
+ const handleLogin = () => {
+    console.log('fser login')
+ }
+
+ const handleCreateAccount = () => {
+   console.log('criar conta')
+ }
+
+ const router =  useRouter()
+ onMounted(()=> {
+    const token = window.localStorage.getItem('token')   
+
+    if (token){
+       router.push({name: 'Feedbacks'})
+    }
+ })
+ 
 </script>
 
